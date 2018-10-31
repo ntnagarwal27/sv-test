9406c0136f65f0d7c5f07628cc3f85040fb17dee
nitin agarwal <nitin@darkhorsesports.in>
2018-10-31 10:22:20 +0530

    added the read me file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sv-test
+
+Test Tool for file renaming according to user
+
+The tool is used to renaming the file ad dsecribed by the user , and it automatically fires up a prompt on download event for attaining input regarding file from user.
+
+Development status
+
+1) at present we are manully firing up the tool using the php command line "php test/index.php"
+
+2)it'll ask to input the file name category, matches it and return true if file found and false if not found.
+
+Yet to be implemented
+
+1)defining the rules
+2)renaming of file according to rules
--- index.php
+++ index.php
@@ -1,35 +1,46 @@
 <?php
 
-// fclose($handle);
-// echo "\n"; 
-// echo "Thank you, continuing...\n";
-
 function check($files){
 
 foreach ($files as $key => $value) {
 	if(is_dir($value))
 	{
        
-     echo "Dir===".$value."<br>";
+    
      echo filemtime($value);
 
-	}else{
-		echo "file===".$value."<br>";
 	}
 }
 }
 echo "Please enter the name";
 $handle = fopen ("php://stdin","r");
-$line = fgets($handle);
-if(trim($line) != ''){
-    echo $line;
-    
+$line = trim(fgets($handle));
+if($line != ''){
+$n = 0;
+$myfile = json_decode(file_get_contents("ffret.json"));
+foreach ($myfile->downloads as $key => $value) {
+	$key = trim($key);
+	if($key === $line)
+	{
+		echo "rules matched for ".$key."\n";
+		$n++;
+	}else
+	{
+		//echo "rules does'nt matched"."<br>";
+	}
+
+}
+if($n==0)
+	{
+	  echo "rules does'nt matched"."\n";	
+	}
+
 }
 
-$fp = fopen('php://stdin', 'r');
-$files = array_slice(scandir('/var/www/html/test'), 2);
-check($files);
-echo "test pahe";
+// $fp = fopen('php://stdin', 'r');
+// $files = array_slice(scandir('/var/www/html/test'), 2);
+// check($files);
+// echo "test pahe";
 
 
 
